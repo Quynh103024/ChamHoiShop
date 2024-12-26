a02045b5ed0bf3fb7e1e8cb0a32ef6714001751e
CongNguyen03 <congminhcs798@gmail.com>
2024-12-26 21:08:56 +0700

    Add files via upload

---
+++ Shop/Areas/Customer/Controllers/CustomerDashboardController.cs
@@ -0,0 +1,32 @@
+﻿using AdminAuthorization.Models;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Models;
+
+namespace Shop.Controllers
+{
+    public class CustomerDashboardController : Controller
+    {
+        private readonly ChamHoiShopContext _context;
+
+        public CustomerDashboardController(ChamHoiShopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Lấy thông tin người dùng hiện tại
+            var userName = HttpContext.Session.GetString("UserName");
+            var user = _context.Users.FirstOrDefault(u => u.UserName == userName);
+
+            if (user == null || user.Role.RoleName != "Customer")
+            {
+                return Unauthorized();
+            }
+
+            // Hiển thị danh sách đơn hàng của người dùng
+            var orders = _context.Orders.Where(o => o.UserID == user.UserId).ToList();
+            return View(orders);
+        }
+    }
+}
